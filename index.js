8d57943445b7df5340fdcb5210e706bffb04d615
Mohit jain <45205171+mohitj2401@users.noreply.github.com>
2021-03-22 17:04:02 +0530

    tested working fine till now

--- index.js
+++ index.js
@@ -1,64 +1,68 @@
-const express=require("express");
+const express = require("express");
 // const popup=require("popups");
-const bodyParser=require("body-parser");
-const request=require("request");
+const bodyParser = require("body-parser");
+const request = require("request");
 // const alert=require("alert-node");
-const app=express();
-const port = process.env.PORT||3000;
+const app = express();
+const port = process.env.PORT || 3000;
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
-app.get("/",function(req,res){
-	res.sendFile(__dirname+"/index.html")
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/index.html")
 });
-app.post("/",function(req,res){
-	var crypto=req.body.crypto;
-	var fait=req.body.fait;
-	var amount=req.body.amount;
-	var baseurl="https://apiv2.bitcoinaverage.com/convert/global"
+app.post("/", function (req, res) {
+	var crypto = req.body.crypto;
+	var fait = req.body.fait;
+	var amount = req.body.amount;
+	var baseurl = "https://apiv2.bitcoinaverage.com/convert/global"
 	// var finalurl=baseurl+crypto+fait;
-	var option={
-		url:baseurl,
-		method:"GET",
-		qs:{
+	var option = {
+		headers: {
+			'x-ba-key': 'Enter Your Key Here',
+		},
+		url: baseurl,
+		method: "GET",
+		qs: {
+
 			from: crypto,
-			to:fait,
-			amount:amount
+			to: fait,
+			amount: amount
 
 		}
 
 	};
 
-	request(option,function(error,response,body){
-		var data=JSON.parse(body);
-		var price=data.price;
-		var currentdate=data.time;
+	request(option, function (error, response, body) {
+		var data = JSON.parse(body);
+		var price = data.price;
+		var currentdate = data.time;
 		console.log(price);
-		if(amount==0){
+		if (amount == 0) {
 			console.log("null");
 			// window.alert("error");
 			res.write("ERRRRRRRRRRRRRRRRRRRRRRRRRRr");
-						
+
 		}
-		else{
-			res.write("<p>The Current date is "+ currentdate +"</p>")
-			res.write("<h1>"+amount+crypto+" is currently "+price+fait+"</h1>");
+		else {
+			res.write("<p>The Current date is " + currentdate + "</p>")
+			res.write("<h1>" + amount + crypto + " is currently " + price + fait + "</h1>");
 		}
-		
+
 		res.send();
 	});
 });
-app.get("/calculater",function(req,res){
+app.get("/calculater", function (req, res) {
 	console.log(__dirname);
-	res.sendFile(__dirname+"/calculater/index.html");
-	
+	res.sendFile(__dirname + "/calculater/index.html");
+
 })
-app.post("/calculater",function(req,res){
-	var a=Number(req.body.num1);
-	var b=Number(req.body.num2);
-	result=a+b;
-	res.send("Result "+result);
+app.post("/calculater", function (req, res) {
+	var a = Number(req.body.num1);
+	var b = Number(req.body.num2);
+	result = a + b;
+	res.send("Result " + result);
 })
 
-app.listen(port,function(){
+app.listen(port, function () {
 	console.log("server is running");
 });
